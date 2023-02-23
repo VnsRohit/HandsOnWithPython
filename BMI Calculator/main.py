6dd0c3d790e39a2e6e99e9bf4441c1f282575a3e
itsNavinSingh <navinkumarsingh371215@gmail.com>
2023-02-23 10:24:59 +0530

    BMI 2.0

--- BMI Calculator/main.py
+++ BMI Calculator/main.py
@@ -1,5 +1,15 @@
-# BMI Calculator
+# BMI calculator 2.0
 height=float(input('Enter your height in m: '))
-weight=float(input('Enter your weight in kg: '))
+weight=int(input('Enter you weight in kg: '))
 bmi=round(weight/(height**2),0)
-print(f'Your BMI is{int(bmi)}')
+if bmi>=35:
+    review='Cliniclly obese'
+elif bmi>=30:
+    review='Obese'
+elif bmi>=25:
+    review='Overweight'
+elif bmi>=18.5:
+    review='Normal Weight'
+else:
+    review='Under Weight'
+print(f'Your BMI is {bmi}, you are {review}')
